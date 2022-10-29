94bc95927b66e2e67149c28dd1d4305e442c67d8
Armaan-grover <97002933+Armaan-grover@users.noreply.github.com>
2022-10-29 08:19:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+function preload(){
+    harryPotterSong =loadSound("music.mp3");
+    PeterPanSong = loadSound("music2.mp3");
+}
+
+function setup(){
+canvas = createCanvas(500,400)
+canvas.center()
+video = createCapture(VIDEO)
+video.hide()
+
+}
+
+function draw(){
+image(video,0,0,500,400)
+}
